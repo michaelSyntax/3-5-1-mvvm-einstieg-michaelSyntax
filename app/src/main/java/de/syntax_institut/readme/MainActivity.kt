2ca46957171d4856a0cb88099c0079abca6394b3
michaelSyntax <michael.karbe@syntax-institut.de>
2024-07-25 16:06:54 +0200

    Change from <Fragment to <androidx.fragment.app.FragmentContainerView

--- app/src/main/java/de/syntax_institut/readme/MainActivity.kt
+++ app/src/main/java/de/syntax_institut/readme/MainActivity.kt
@@ -6,11 +6,17 @@ import de.syntax_institut.readme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // new changes
+
+
     }
 }
